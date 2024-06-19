95f2561678d318ccb1f8a0c40ab812b4d2e6b841
Justin <62445349+zenarcher007@users.noreply.github.com>
2024-06-19 16:29:48 -0500

    Add better error handling, on() wait until active, context manager

--- X11Renderer.py
+++ X11Renderer.py
@@ -4,6 +4,7 @@ from collections import deque
 import signal
 import subprocess
 import numpy as np
+import time
 os.environ['MUJOCO_GL'] = 'egl' # Tells mujoco to use an Nvidia GPU for display buffer rendering. Remove this if this does not apply to you
 # To install dependencies and other useful tools on Ubuntu, open a terminal and type:
 # sudo apt update -y && sudo apt install -y xvfb xterm x11-apps imagemagick xdotool
@@ -15,49 +16,89 @@ class DataWaiter:
     self.data = None
     self.lock = threading.Lock()
     self.cv = threading.Condition(self.lock)
+    self.activated = False
 
   def setData(self, data):
     with self.lock:
       self.data = data
+      self.activated = True
       self.cv.notify_all()
 
   def getData(self):
-    while self.data is None:
+    while self.activated == False:
       with self.lock:
         self.cv.wait()
+    self.activated = False
     return self.data
 
-# Example:
-# xbd = X11Renderer(width = 1024, height = 768, display_num = 0)
-# xbd.on()
-# ...
-# plt.imshow(xbd.render())
-# ...
-# xbd.off()
 class X11Renderer:
-  def __init__(self, width, height, display_num=0):
+  def __init__(self, width, height, display=0):
     self.width = width
     self.height = height
-    self.display_num = display_num
+    self.display_num = display
     self._xvfb_thread = None
     self._xvfb_proc = None
 
   def __del__(self):
     self.off()
+
+  # Context manager that you can use with the "with" keyword
+  def __enter__(self):
+    self.on()
+    return self
+
+  def __exit__(self, exc_type, exc_value, traceback):
+    self.off()
+    if exc_type is not None:
+      raise exc_type(exc_value).with_traceback(traceback)
   
   def _xvfb_thread_func(self, dataptr: DataWaiter):
-    proc = subprocess.Popen(['Xvfb', f':{self.display_num}', '-screen', f'{self.display_num}', f'{self.width}x{self.height}x24'])
-    dataptr.setData(proc)
-    proc.communicate()
+    if self.isDisplayInUse():
+      r = RuntimeError(f"Display :{self.display_num} is not available!")
+      dataptr.setData(r)
+      raise r
+      
+    try:
+      cmd = ['Xvfb', f':{self.display_num}', '-screen', f'{self.display_num}', f'{self.width}x{self.height}x24']
+      proc = subprocess.Popen(cmd)
+      dataptr.setData(proc)
+      proc.communicate()
+      if proc.returncode != 0 and proc.returncode != -9:
+        raise subprocess.CalledProcessError(cmd = cmd, returncode = proc.returncode)
+    except Exception as e:
+      dataptr.setData(e)
+      raise e
+    finally: # Turn "off"
+      self._xvfb_thread = None
+      self._xvfb_proc = None
+
+  def isDisplayInUse(self):
+    #proc = 
+    #proc.stderr = open('/dev/null')
+    #proc.stdout = open('/dev/null')
+    try:
+      subprocess.run(['xdpyinfo', '-display', f':{self.display_num}'], check = True, stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
+      #proc.communicate()
+      return True
+    except subprocess.CalledProcessError as e:
+      return False
 
   # Starts the Xvfb thread. If it is already present, does nothing.
-  def on(self):
+  def on(self, wait = True):
     if self._xvfb_thread is not None:
       return
-    dataptr = DataWaiter() # Retrieve the PID by sending in a deque, and reading from it after starting the process
+    dataptr = DataWaiter() # Retrieve the PID by sending in a datawaiter, and reading from it after starting the process
     self._xvfb_thread = threading.Thread(target = self._xvfb_thread_func, args=(dataptr,), daemon=True)
     self._xvfb_thread.start()
-    self._xvfb_proc = dataptr.getData() # Should hang until there is an item present in the queue
+    while wait:
+      if self.isDisplayInUse() or not self._xvfb_thread.is_alive():
+        break
+      else:
+        time.sleep(0.2)
+    data = dataptr.getData() # Should hang until there is an item present in the queue
+    if isinstance(data, Exception):
+      raise CalledProcessError from data
+    self._xvfb_proc = data
     assert(self._xvfb_proc is not None and self._xvfb_thread is not None)
   
   # Stops the Xvfb thread. If it is not present, does nothing.
@@ -72,7 +113,7 @@ class X11Renderer:
   # Captures and returns the current X11 frame as a numpy array (viewable from plt.show())
   def render(self):
     if self._xvfb_thread is None:
-      raise RuntimeError("Backdrop thread is not running!")
+      raise RuntimeError("X11 thread is not running!")
     out = None
     with subprocess.Popen(['sh', '-c', f'DISPLAY=:{self.display_num} xwd -root -silent | convert xwd:- -depth 8 rgb:-'], stdout=subprocess.PIPE) as proc:
       out = proc.stdout.read()
